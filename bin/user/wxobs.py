43a6c49a6b896a8f3b62f49f927cab5319424507
Glenn McKechnie <glennmckechnie@gmail.com>
2018-01-31 21:56:21 +1100

    add rsync for remote transfers

--- bin/user/wxobs.py
+++ bin/user/wxobs.py
@@ -5,11 +5,19 @@
 #    Mistakes are mine, corrections and or improvements welcomed
 #       https://github.com/glennmckechnie/weewx-wxobs
 #
+#    rsync code based on weeutil/rsyncupload.py by
+#       Will Page <companyguy@gmail.com> and
+#
 #    See the file LICENSE.txt for your full rights.
 #
 #
 
 import syslog
+import subprocess
+import time
+import errno
+
+import weewx.engine
 
 from weeutil.weeutil import to_bool
 from weewx.cheetahgenerator import SearchList
@@ -28,6 +36,112 @@ def logerr(msg):
 def logdbg(msg):
     logmsg(syslog.LOG_DEBUG, msg)
 
+def rsync(rsync_user, rsync_remote, rsync_loc_dir, rsync_rem_str, wxobs_debug, log_success):
+
+    t1 = time.time()
+    # construct the command argument
+    cmd = ['rsync']
+    #cmd.extend(["-ac"])
+    cmd.extend(["-tOJrl"])
+    # provide some stats on the transfer
+    cmd.extend(["--stats"])
+    cmd.extend(["--compress"])
+    cmd.extend([rsync_loc_dir])
+    cmd.extend([rsync_rem_str])
+
+    try:
+        # perform the actual rsync transfer...
+        if wxobs_debug >= 2:
+            logdbg("rsync cmd is ... %s" % (cmd))
+        #loginf("rsync cmd is ... %s" % (cmd))
+        rsynccmd = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout = rsynccmd.communicate()[0]
+        stroutput = stdout.encode("utf-8").strip()
+    except OSError, e:
+            #print "EXCEPTION"
+        if e.errno == errno.ENOENT:
+            logerr("wxobs-rsync: rsync does not appear to be installed on this system. \
+                   (errno %d, \"%s\")" % (e.errno, e.strerror))
+        raise
+
+    # we have some output from rsync so generate an appropriate message
+    if stroutput.find('rsync error:') < 0:
+        # no rsync error message so parse rsync --stats results
+        rsyncinfo = {}
+        for line in iter(stroutput.splitlines()):
+            if line.find(':') >= 0:
+                (n, v) = line.split(':', 1)
+                rsyncinfo[n.strip()] = v.strip()
+        # get number of files and bytes transferred and produce an
+        # appropriate message
+        try:
+            if 'Number of regular files transferred' in rsyncinfo:
+                N = rsyncinfo['Number of regular files transferred']
+            else:
+                N = rsyncinfo['Number of files transferred']
+
+            Nbytes = rsyncinfo['Total transferred file size']
+            if N is not None and Nbytes is not None:
+                rsync_message = "rsync'd %s files (%s) in %%0.2f seconds" % (N, Nbytes)
+            else:
+                rsync_message = "rsync executed in %0.2f seconds"
+        except:
+            rsync_message = "rsync :exception raised: executed in %0.2f seconds"
+    else:
+        # suspect we have an rsync error so tidy stroutput
+        # and display a message
+        stroutput = stroutput.replace("\n", ". ")
+        stroutput = stroutput.replace("\r", "")
+        # Attempt to catch a few errors that may occur and deal with them
+        # see man rsync for EXIT VALUES
+        rsync_message = "rsync command failed after %0.2f seconds (set 'wxobs_debug = 1'),"
+        if "code 1)" in stroutput:
+            if wxobs_debug >= 2:
+                logerr("wxobs-rsync: rsync code 1 - %s" % stroutput)
+            rsync_message = "syntax error in rsync command - set debug = 1 - ! FIX ME !"
+            loginf("wxobs-rsync:  ERR %s " % (rsync_message))
+            rsync_message = "code 1, syntax error, failed rsync executed in %0.2f seconds"
+
+        elif ("code 23" and "Read-only file system") in stroutput:
+            # read-only file system
+            # sadly, won't be detected until after first succesful transfer
+            # but it's useful then!
+            if wxobs_debug >= 2:
+                logerr("wxobs-rsync: rsync code 23 - %s" % stroutput)
+            loginf("ERR Read only file system ! FIX ME !")
+            rsync_message = "code 23, read-only, rsync failed executed in %0.2f seconds"
+        elif ("code 23" and "link_stat") in stroutput:
+            # likely to be that a local path doesn't exist - possible typo?
+            if wxobs_debug >= 2:
+                logdbg("wxobs-rsync: rsync code 23 found %s" % stroutput)
+            rsync_message = "rsync code 23 : is %s correct? ! FIXME !" % (rsync_loc_dir)
+            loginf("wxobs-rsync:  ERR %s " % rsync_message)
+            rsync_message = "code 23, link_stat, rsync failed executed in %0.2f seconds"
+
+        elif ("code 12") and ("Permission denied") in stroutput:
+            if wxobs_debug >= 2:
+                logdbg("wxobs-rsync: rsync code 12 - %s" % stroutput)
+            rsync_message = "Permission error in rsync command, probably remote \
+                             authentication ! FIX ME !"
+            loginf("wxobs-rsync:  ERR %s " % (rsync_message))
+            rsync_message = "code 12, rsync failed, executed in % 0.2f seconds"
+        elif ("code 12") and ("No route to host") in stroutput:
+            if wxobs_debug >= 2:
+                logdbg("wxobs-rsync: rsync code 12 - %s" % stroutput)
+            rsync_message = "No route to host error in rsync command ! FIX ME !"
+            loginf("wxobs-rsync:  ERR %s " % (rsync_message))
+            rsync_message = "code 12, rsync failed, executed in % 0.2f seconds"
+        else:
+            logerr("ERROR: wxobs-rsync: [%s] reported this error: %s" % (cmd, stroutput))
+
+    if log_success:
+        if wxobs_debug == 0:
+            to = ''
+            rsync_rem_str = ''
+        else:
+            to = ' to '
+        t2= time.time()
+        loginf("wxobs-rsync: %s" % rsync_message % (t2-t1) + to + rsync_rem_str)
 
 class wxobs(SearchList):
 
@@ -38,8 +152,14 @@ class wxobs(SearchList):
         This is a minimal SLE - all we want is to make the php configuration
         easier by transferring the database values as used by weewx, plus a few
         other variables.
+        It has since expanded to allow the transfer of the sqlite database
+        by using rsync, providing the [Remote] section of the config is populated
 
         In wxobs/skin.conf:
+
+        inlcude_path: the directory where the php include file will be stored
+        this holds the database configuration as sourced from weewx.conf
+
         ext_interval: is the spacing between records; 1800 is a half-hour
         and is the default
 
@@ -60,8 +180,15 @@ class wxobs(SearchList):
         The default is to use windchill.
         Keep it to the group_degrees (because the label is hard coded in.)
 
+        [[Remote]]
+        This is used to transfer the include file and the database to a remote
+        machine (as used in weewx.conf [FTP] or [RSYNC] section)
+        rsync_user = user_name for rsync command
+        rsync_machine = ip address of remote nachine
+
+        [[RainTiming]]
         shift_rain: For rain accounting times other than midnight to midnight
-        set this to True 
+        set this to True
         If no other options are given the accounting time will be the australian
         rain day which starts at 9 a.m.
         default is false - start at midnight 00:00:00 through to the next day.
@@ -85,10 +212,12 @@ class wxobs(SearchList):
         4 only for delta-T unit conversion calcs (verbose) - if enabled
         5 only for ordinalCompass conversion calcs (N, NE...CALM) (verbose)
 
+        [[DeltaT]]
         calculate_deltaT: Whether to generate deltaT for the report page.
         Default is not to generate that data.
         This is a switch (boolean) to turn it on.
 
+        [[PHPUnits]]
         tempConvert:
         speedConvert:
         pressConvert:
@@ -102,6 +231,7 @@ class wxobs(SearchList):
         The default is to perform no conversion, to accept the units as they
         are.
         """
+
 
         self.wxobs_version = wxobs_version
         self.wxobs_debug = int(self.generator.skin_dict['wxobs'].get(
@@ -149,6 +279,19 @@ class wxobs(SearchList):
         self.targ_unit = self.generator.config_dict['StdConvert'].get(
             'target_unit')
 
+        # used for sqlite databases transfer to remote machines
+        self.rsync_user = self.generator.skin_dict['wxobs']['Remote'].get(
+            'rsync_user', None)
+        self.rsync_remote = self.generator.skin_dict['wxobs']['Remote'].get(
+            'rsync_machine', None)
+        self.log_success = to_bool(self.generator.skin_dict['wxobs']['Remote'].get(
+            'log_success', True))
+
+
+
+
+
+        # prepare the database details and write the include file
         def_dbase = self.generator.config_dict['DataBindings'] \
             ['wx_binding'].get('database')
         if self.wxobs_debug >= 5:
@@ -188,17 +331,40 @@ class wxobs(SearchList):
                 loginf("sqlite database is %s, %s, %s" % (
                     self.sq_dbase, self.sq_root, self.sqlite_db))
 
+
         # phpinfo.php shows include_path as .:/usr/share/php, we'll put it
         # in there and hopefully that will work for most users.
-        # I iuse/prefer /tmp/wxobs_inc.inc but perhaps that only works for me?
-        self.include_file = self.generator.skin_dict['wxobs'].get(
-            'include_file', '/usr/share/php/wxobs_incl.inc')
+        # I use/prefer /tmp/wxobs_inc.inc
+        inc_file = 'wxobs_incl.inc'
+        inc_path = self.generator.skin_dict['wxobs'].get(
+            'include_path', '/usr/share/php')
+        self.include_file = ("%s/%s" % (inc_path, inc_file))
 
         php_inc = open(self.include_file, 'w')
         php_inc.writelines(v_al)
         php_inc.close()
 
-if __name__ == '__main__':
-    # Hmmm!
-    # use wee_reports instead, see the inline comments above.
-    pass
+        # for testing, bypass mysql lockout
+        def_dbase = 'archive_sqlite'
+        self.dbase = 'sqlite'
+        self.sq_dbase = self.generator.config_dict['Databases'] \
+            [def_dbase].get('database_name')
+        self.sq_root = self.generator.config_dict['DatabaseTypes'] \
+            ['SQLite'].get('SQLITE_ROOT')
+        self.sqlite_db = ("%s/%s" %(self.sq_root, self.sq_dbase))
+
+        # use rsync to transfer database remotely, ONLY if requested
+        if (def_dbase == 'archive_sqlite') and (self.rsync_user is not None) and (self.rsync_remote is not None):
+            # construct strings for remote transfers
+            # database transfer
+            if len(self.rsync_user.strip()) > 0:
+                db_loc_dir = "%s" % (self.sqlite_db)
+                db_rem_str = "%s@%s:%s" % (self.rsync_user, self.rsync_remote, self.sq_root)
+            rsync(self.rsync_user, self.rsync_remote, db_loc_dir, db_rem_str,
+                  self.wxobs_debug, self.log_success)
+            # include file transfer
+            if len(self.rsync_user.strip()) > 0:
+                inc_loc_dir = "%s" % (self.include_file)
+                inc_rem_str = "%s@%s:%s" % (self.rsync_user, self.rsync_remote, inc_path)
+            rsync(self.rsync_user, self.rsync_remote, inc_loc_dir, inc_rem_str,
+                  self.wxobs_debug, self.log_success)
