a7f3c324bbc10df2b580891c46ac254704dcfd5f
Glenn McKechnie <glennmckechnie@gmail.com>
2018-03-14 20:36:16 +1100

    missing database symlink bump version

--- bin/user/wxobs.py
+++ bin/user/wxobs.py
@@ -23,7 +23,7 @@ import weewx.engine
 from weeutil.weeutil import to_bool
 from weewx.cheetahgenerator import SearchList
 
-wxobs_version = "0.6.4"
+wxobs_version = "0.6.5"
 
 def logmsg(level, msg):
     syslog.syslog(level, '%s' % msg)
